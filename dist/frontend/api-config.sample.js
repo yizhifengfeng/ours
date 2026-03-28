6fefe11ad11a9a4750975ead47de6f121c76758d
yizhifengfeng <2118455110@qq.com>
2026-03-28 22:07:06 +0800

    Delete dist directory

--- dist/frontend/api-config.sample.js
+++
@@ -1,8 +0,0 @@
-;(function () {
-    'use strict';
-    // 前后端分开部署时：复制本文件为同目录下的 api-config.js，填写后端地址（不要末尾 /）
-    // 并在各 HTML 里、在其他 <script> 之前加入：<script src="api-config.js"></script>
-    if (typeof window !== 'undefined' && !window.API_BASE_URL) {
-        window.API_BASE_URL = 'https://你的后端项目.vercel.app';
-    }
-})();
